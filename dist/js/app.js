129462b24e47cd0b38a6a1391b6d882323fa98c0
Joaquim Chianca <joaquimchianca@gmail.com>
2023-04-28 22:08:05 -0300

    Refatoração do modelo Negociacao

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,3 @@
-import { Negociacao } from './models/negociacao.js'
-
-const negociacao = new Negociacao(new Date(), 15, 30)
-
-console.log(negociacao.quantidade)
+import { Negociacao } from './models/negociacao.js';
+const negociacao = new Negociacao(new Date(), 15, 30);
+console.log(negociacao.quantidade);
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,22 +1,13 @@
 export class Negociacao {
-    #data
-    #quantidade
-    #valor
-
     constructor(data, quantidade, valor) {
-        this.#data = data
-        this.#quantidade = quantidade
-        this.#valor = valor
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
-
-    get data() { return this.#data }
-
-    get quantidade() { return this.#quantidade }
-
-    get valor() { return this.#valor }
-
-    get volume() { 
-        return this.#quantidade * this.#valor
+    get data() { return this._data; }
+    get quantidade() { return this._quantidade; }
+    get valor() { return this._valor; }
+    get volume() {
+        return this._quantidade * this._valor;
     }
-
 }
